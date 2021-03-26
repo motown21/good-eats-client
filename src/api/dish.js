7739276f418e6e772dce540763bd1623be3c7877
Monique <monique.townson@gmail.com>
2021-03-26 10:59:04 -0700

    initial commit

---
+++ src/api/dish.js
@@ -0,0 +1,55 @@
+import apiUrl from '../apiConfig'
+
+import axios from 'axios'
+
+export const dishIndex = user => {
+  return axios({
+    url: apiUrl + '/dishes',
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+
+export const dishCreate = (dish, user) => {
+  return axios({
+    url: apiUrl + '/dishes',
+    method: 'dish',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: { dish }
+  })
+}
+
+// get a single dish
+export const dishShow = (id, user) => {
+  return axios({
+    url: apiUrl + '/dishes/' + id,
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+
+export const dishDelete = (id, user) => {
+  return axios({
+    url: apiUrl + '/dishes/' + id,
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+export const dishUpdate = (id, dish, user) => {
+  return axios({
+    url: apiUrl + '/dishes/' + id,
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: { dish }
+  })
+}
